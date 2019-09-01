3938ef63cb5a575281cab97cb6cab9eccb97d19e
avc94 <>
2019-09-01 15:48:47 +0300

    Added custom separator and text label input for current script session

--- make_photos.py
+++ make_photos.py
@@ -16,13 +16,15 @@ def main():
     dist = float(input("Move straight for (meters): "))
     dist *= 1000  # convert to mm
     dist = int(dist)
+    label = input("Label for that session: ")
+    sep = " "
 
     # offset = int(input("Take image every (mm): "))
     offset = 75  # 151 mm is full AOI
 
     for B in range(0, dist, offset):
         image = pca.get_image()
-        cv.imwrite("gathered_data/" + str(datetime.datetime.now()) + " - " + str(B) + ".jpg", image)
+        cv.imwrite("gathered_data/" + str(datetime.datetime.now()) + sep + str(B) + sep + label + ".jpg", image)
 
         sma.nav_move_forward(int(offset * config.ONE_MM_IN_SMOOTHIE), config.B_F_MAX)
 
@@ -31,7 +33,7 @@ def main():
 
     # take last image
     image = pca.get_image()
-    cv.imwrite("/gathered_data/" + str(datetime.datetime.now()) + " - last" + ".jpg", image)
+    cv.imwrite("/gathered_data/" + str(datetime.datetime.now()) + sep + "last" + sep + label + ".jpg", image)
 
     print("Done.")
 
