6bde5ee87af017eaf8bd5bd70a18550ae0ac09bc
avc94 <>
2019-09-11 19:02:36 +0300

    Added validation to custom_move_for func, implemented custom_move_to func, removed old direction calculations func, some small editing and improvements

--- adapters.py
+++ adapters.py
@@ -100,8 +100,8 @@ class SmoothieAdapter:
 
     def get_smoothie_current_coordinates(self):
         with self._sync_locker.acquire():
-            self._smc.write("M114")
-            res = self._smc.read_some()
+            self._smc.write("M114.2")
+            res = self._smc.read_some()  # unite 2 msg into 1
 
             """
             Answers:
@@ -131,17 +131,59 @@ class SmoothieAdapter:
 
         with self._sync_locker.acquire():
             g_code = "G0"
+
             if X is not None:
+                error_msg = self.validate_value(0, F, "F", config.XY_F_MIN, config.XY_F_MAX, "XY_F_MIN", "XY_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._x_cur.value, X, "X", config.X_MIN, config.X_MAX, "X_MIN", "X_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " X" + str(X)
+
             if Y is not None:
+                error_msg = self.validate_value(0, F, "F", config.XY_F_MIN, config.XY_F_MAX, "XY_F_MIN", "XY_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._y_cur.value, Y, "Y", config.Y_MIN, config.Y_MAX, "Y_MIN", "Y_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " Y" + str(Y)
+
             if Z is not None:
+                error_msg = self.validate_value(0, F, "F", config.Z_F_MIN, config.Z_F_MAX, "Z_F_MIN", "Z_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._z_cur.value, Z, "Z", config.Z_MIN, config.Z_MAX, "Z_MIN", "Z_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " Z" + str(Z)
+
             if A is not None:
+                error_msg = self.validate_value(0, F, "F", config.A_F_MIN, config.A_F_MAX, "A_F_MIN", "A_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._a_cur.value, A, "A", config.A_MIN, config.A_MAX, "A_MIN", "A_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " A" + str(A)
+
             if B is not None:
+                error_msg = self.validate_value(0, F, "F", config.B_F_MIN, config.B_F_MAX, "B_F_MIN", "B_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._b_cur.value, B, "B", config.B_MIN, config.B_MAX, "B_MIN", "B_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " B" + str(B)
+
             if C is not None:
+                error_msg = self.validate_value(0, F, "F", config.C_F_MIN, config.C_F_MAX, "C_F_MIN", "C_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(self._c_cur.value, C, "C", config.C_MIN, config.C_MAX, "C_MIN", "C_MAX")
+                if error_msg:
+                    return error_msg
                 g_code += " C" + str(C)
 
             self._smc.write(g_code)
@@ -165,10 +207,103 @@ class SmoothieAdapter:
             if C is not None:
                 with self._c_cur.get_lock():
                     self._c_cur.value += C
+        return None
 
-    def custom_move_to(self, X=None, Y=None, Z=None, A=None, B=None, C=None):
+    def custom_move_to(self, F: int, X=None, Y=None, Z=None, A=None, B=None, C=None):
         """Movement to the specified position"""
-        raise NotImplementedError("This option is not available yet.")
+
+        if self._check_arg_types(type(None), X, Y, Z, A, B, C):
+            raise TypeError("at least one axis shouldn't be None")
+        if not self._check_arg_types([int, type(None)], X, Y, Z, A, B, C):
+            raise TypeError("incorrect axis coordinates value(s) type(s)")
+        if not self._check_arg_types([int], F):
+            raise TypeError("incorrect force F value type")
+
+        with self._sync_locker.acquire():
+            g_code = "G0"
+
+            if X is not None:
+                error_msg = self.validate_value(0, F, "F", config.XY_F_MIN, config.XY_F_MAX, "XY_F_MIN", "XY_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, X, "X", config.X_MIN, config.X_MAX, "X_MIN", "X_MAX")
+                if error_msg:
+                    return error_msg
+                smc_x = X - self._x_cur.value
+                g_code += " X" + str(smc_x)
+
+            if Y is not None:
+                error_msg = self.validate_value(0, F, "F", config.XY_F_MIN, config.XY_F_MAX, "XY_F_MIN", "XY_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, Y, "Y", config.Y_MIN, config.Y_MAX, "Y_MIN", "Y_MAX")
+                if error_msg:
+                    return error_msg
+                smc_y = Y - self._y_cur.value
+                g_code += " Y" + str(smc_y)
+
+            if Z is not None:
+                error_msg = self.validate_value(0, F, "F", config.Z_F_MIN, config.Z_F_MAX, "Z_F_MIN", "Z_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, Z, "Z", config.Z_MIN, config.Z_MAX, "Z_MIN", "Z_MAX")
+                if error_msg:
+                    return error_msg
+                smc_z = Z - self._z_cur.value
+                g_code += " Z" + str(smc_z)
+
+            if A is not None:
+                error_msg = self.validate_value(0, F, "F", config.A_F_MIN, config.A_F_MAX, "A_F_MIN", "A_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, A, "A", config.A_MIN, config.A_MAX, "A_MIN", "A_MAX")
+                if error_msg:
+                    return error_msg
+                smc_a = A - self._a_cur.value
+                g_code += " A" + str(smc_a)
+
+            if B is not None:
+                error_msg = self.validate_value(0, F, "F", config.B_F_MIN, config.B_F_MAX, "B_F_MIN", "B_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, B, "B", config.B_MIN, config.B_MAX, "B_MIN", "B_MAX")
+                if error_msg:
+                    return error_msg
+                smc_b = B - self._b_cur.value
+                g_code += " B" + str(smc_b)
+
+            if C is not None:
+                error_msg = self.validate_value(0, F, "F", config.C_F_MIN, config.C_F_MAX, "C_F_MIN", "C_F_MAX")
+                if error_msg:
+                    return error_msg
+                error_msg = self.validate_value(0, C, "C", config.C_MIN, config.C_MAX, "C_MIN", "C_MAX")
+                if error_msg:
+                    return error_msg
+                smc_c = C - self._c_cur.value
+                g_code += " C" + str(smc_c)
+
+            self._smc.write(g_code)
+            self._smc.read_until("ok\r\n")
+
+            if X is not None:
+                with self._x_cur.get_lock():
+                    self._x_cur.value += smc_x
+            if Y is not None:
+                with self._y_cur.get_lock():
+                    self._y_cur.value += smc_y
+            if Z is not None:
+                with self._z_cur.get_lock():
+                    self._z_cur.value += smc_z
+            if A is not None:
+                with self._a_cur.get_lock():
+                    self._a_cur.value += smc_a
+            if B is not None:
+                with self._b_cur.get_lock():
+                    self._b_cur.value += smc_b
+            if C is not None:
+                with self._c_cur.get_lock():
+                    self._c_cur.value += smc_z
+        return None
 
     def nav_move_forward(self, distance: int, F: int):
         with self._b_cur.get_lock():
@@ -177,6 +312,7 @@ class SmoothieAdapter:
             self._smc.read_until("ok\r\n")
 
     def nav_move_backward(self, distance: int, F: int):
+
         raise NotImplementedError("This option is not available yet.")
 
     def nav_turn_wheels_for(self, value: int, F: int):
@@ -201,7 +337,7 @@ class SmoothieAdapter:
             if error_msg:
                 return error_msg
 
-            smc_a = self._calc_coords_diff(self._a_cur.value, destination)
+            smc_a = destination - self._a_cur.value
             self._smc.write("G0 A{0} F{1}".format(smc_a, F))
             self._a_cur.value += smc_a
             self._smc.read_until("ok\r\n")
@@ -228,8 +364,7 @@ class SmoothieAdapter:
             with self._y_cur.get_lock():
                 # calc cork center coords and xy movement values for smoothie g-code
                 center_x, center_y = int(config.X_MAX / 2), int(config.Y_MAX / 2)
-                smc_x, smc_y = self._calc_coords_diff(self._x_cur.value, center_x),\
-                               self._calc_coords_diff(self._y_cur.value, center_y)
+                smc_x, smc_y = center_x - self._x_cur.value, center_y - self._y_cur.value
                 g_code = "G0 X" + str(smc_x) + " Y" + str(smc_y) + " F" + str(config.XY_F_MAX)
                 self._smc.write(g_code)
                 self._x_cur.value += smc_x
@@ -279,7 +414,9 @@ class SmoothieAdapter:
 
     @staticmethod
     def validate_value(cur_value, value, key_label, key_min, key_max, key_min_label, key_max_label):
-        """For F current_value must be 0"""
+        """Returns None if value is ok, error message otherwise.
+
+        For force F current_value must be 0"""
 
         if cur_value + value > key_max:
             return "Value {0} for {1} goes beyond max acceptable range of {3} = {2}, " \
@@ -288,10 +425,6 @@ class SmoothieAdapter:
             return "Value {0} for {1} goes beyond min acceptable range of {3} = {2}, " \
                        .format(value, key_label, key_min, key_min_label)
         return None
-
-    def _calc_coords_diff(self, current: int, destination: int):
-        """Returns pos. or neg. value for motion"""
-        return -abs(current - destination) if current > destination else abs(current - destination)
 
 
 class PiCameraAdapter:
