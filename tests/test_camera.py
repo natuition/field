df763c05aec2b7398dd727c7204826355ae42ba7
avc94 <>
2019-11-10 17:23:05 +0200

    Added tests dir

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,14 +1,18 @@
 import adapters
 import cv2 as cv
+import time
+
+delay = 3
+image_count = 20
 
 
 def main():
     cam = adapters.CameraAdapterIMX219_170()
 
-    for i in range(3):
-        input("Press enter to get an image")
+    for i in range(image_count):
         img = cam.get_image()
         cv.imwrite("test_camera_" + str(i) + ".jpg", img)
+        time.sleep(delay)
 
 
 if __name__ == '__main__':
