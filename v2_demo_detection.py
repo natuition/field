02d83af1091181730f7aa546609e32edd45f7e92
avc94 <>
2019-11-11 12:35:38 +0200

    Added context manager usage for camera

--- v2_demo_detection.py
+++ v2_demo_detection.py
@@ -44,48 +44,48 @@ def main():
     print("Loading YOLO...")
     det = detection.YoloOpenCVDetection()
     print("Loading camera...")
-    camera = adapters.CameraAdapterIMX219_170()
-    print("Camera warming up...")
-    time.sleep(3)
-    print("Loading complete.")
+    with adapters.CameraAdapterIMX219_170() as camera:
+        print("Camera warming up...")
+        time.sleep(3)
+        print("Loading complete.")
 
-    counter = 1
-    sep = " "
+        counter = 1
+        sep = " "
 
-    # make photos
-    while True:
-        action = input("Type empty string (just hit enter key) to make photo, type anything to begin detection: ")
-        if action != "":
-            break
+        # make photos
+        while True:
+            action = input("Type empty string (just hit enter key) to make photo, type anything to begin detection: ")
+            if action != "":
+                break
 
-        image = camera.get_image()
-        cv.imwrite(config.INPUT_IMG_DIR + str(datetime.datetime.now()) + sep + str(counter) + ".jpg", image)
-        counter += 1
+            image = camera.get_image()
+            cv.imwrite(config.INPUT_IMG_DIR + str(datetime.datetime.now()) + sep + str(counter) + ".jpg", image)
+            counter += 1
 
-    # detect plants on that photos
-    images = glob.glob(config.INPUT_IMG_DIR + "*.jpg")
-    counter = 1
-    for file_path in images:
-        print("Processing " + str(counter) + " of " + str(len(images)) + " images")
-        counter += 1
+        # detect plants on that photos
+        images = glob.glob(config.INPUT_IMG_DIR + "*.jpg")
+        counter = 1
+        for file_path in images:
+            print("Processing " + str(counter) + " of " + str(len(images)) + " images")
+            counter += 1
 
-        img = cv.imread(file_path)
-        img_y_c, img_x_c = int(img.shape[0] / 2), int(img.shape[1] / 2)
+            img = cv.imread(file_path)
+            img_y_c, img_x_c = int(img.shape[0] / 2), int(img.shape[1] / 2)
 
-        boxes = det.detect(img)
+            boxes = det.detect(img)
 
-        for i in range(len(boxes)):
-            detection.draw_box(img, boxes[i])
+            for i in range(len(boxes)):
+                detection.draw_box(img, boxes[i])
 
-        img = draw_zones_circle(img, img_x_c, img_y_c, undistorted_zone_radius, working_zone_radius)
+            img = draw_zones_circle(img, img_x_c, img_y_c, undistorted_zone_radius, working_zone_radius)
 
-        # put results in the input folder
-        # cv.imwrite(file_path[:-4] + " - result.jpg", img)
+            # put results in the input folder
+            # cv.imwrite(file_path[:-4] + " - result.jpg", img)
 
-        file_name = file_path.split("\\")[-1]
-        cv.imwrite(config.OUTPUT_IMG_DIR + "Result " + file_name, img)
+            file_name = file_path.split("\\")[-1]
+            cv.imwrite(config.OUTPUT_IMG_DIR + "Result " + file_name, img)
 
-    print("Done!")
+        print("Done!")
 
 
 if __name__ == "__main__":
