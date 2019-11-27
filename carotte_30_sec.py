568ef7a3466be11642ad7c8659a07b9e52e3976f
avc94 <>
2019-11-27 10:44:51 +0200

    Added carotte script with infinite loop with 30 sec delay

---
+++ carotte_30_sec.py
@@ -0,0 +1,37 @@
+from config import config
+import adapters
+import time
+
+
+def main():
+    print("Loading smoothie adapter")
+    smoothie = adapters.SmoothieAdapter(config.SMOOTHIE_HOST)
+
+    while True:
+        print("Calibrating cork")
+        smoothie.ext_calibrate_cork()
+        smoothie.wait_for_all_actions_done()
+
+        print("Aligning cork to center")
+        res = smoothie.ext_align_cork_center(config.XY_F_MAX)
+        smoothie.wait_for_all_actions_done()
+        print(res)
+
+        print("Extracting, cork down")
+        # extraction, cork down
+        res = smoothie.custom_move_for(config.Z_F_MAX, Z=-30)
+        smoothie.wait_for_all_actions_done()
+        print(res)
+
+        # extraction, cork up
+        print("Extracting, cork up")
+        res = smoothie.ext_cork_up()
+        smoothie.wait_for_all_actions_done()
+        print(res)
+
+        print("30 sec delay...")
+        time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
