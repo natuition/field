43eb0cab2ba1d1f56bbe654f6b372da3e922e045
avc94 <>
2019-12-27 14:54:29 +0200

    Added 5-points markup tool mode for make photos manual script

--- v2_make_photos_manual.py
+++ v2_make_photos_manual.py
@@ -5,6 +5,21 @@ import os
 import time
 
 
+def markup_5_points(image):
+    img_y_c, img_x_c = int(image.shape[0] / 2), int(image.shape[1] / 2)
+    # center
+    image = cv.circle(image, (img_x_c, img_y_c), 3, (0, 0, 255), thickness=3)
+    # top
+    image = cv.circle(image, (img_x_c, img_y_c - 400), 3, (0, 0, 255), thickness=3)
+    # bottom
+    image = cv.circle(image, (img_x_c, img_y_c + 400), 3, (0, 0, 255), thickness=3)
+    # left
+    image = cv.circle(image, (img_x_c - 400, img_y_c), 3, (0, 0, 255), thickness=3)
+    # right
+    image = cv.circle(image, (img_x_c + 400, img_y_c), 3, (0, 0, 255), thickness=3)
+    return image
+
+
 def main():
     output_dir = "gathered_data/"
     if not os.path.exists(output_dir):
@@ -16,24 +31,31 @@ def main():
             print("Successfully created the directory %s " % output_dir)
 
     print("Loading...")
-    camera = adapters.CameraAdapterIMX219_170()
-    time.sleep(2)
-    print("Loading complete.")
+    with adapters.CameraAdapterIMX219_170() as camera:
+        time.sleep(2)
+        print("Loading complete.")
 
-    label = input("Please type label, which should be added to photos: ")
-    sep = " "
-    counter = 1
+        draw_markup = input(
+            "Type t to draw markup points on images, type empty string (just hit enter key) to leave images clean: ")
+        draw_markup = True if draw_markup == "t" else False
 
-    while True:
-        action = input("Type empty string (just hit enter key) to get an image, type anything to stop: ")
-        if action != "":
-            break
+        label = input("Please type label, which should be added to photos: ")
 
-        image = camera.get_image()
-        cv.imwrite(output_dir + label + sep + str(counter) + sep + str(datetime.datetime.now()) + ".jpg", image)
-        counter += 1
+        sep = " "
+        counter = 1
 
-    print("Done.")
+        while True:
+            action = input("Type empty string (just hit enter key) to get an image, type anything to stop: ")
+            if action != "":
+                break
+
+            image = camera.get_image()
+            if draw_markup:
+                image = markup_5_points(image)
+            cv.imwrite(output_dir + label + sep + str(counter) + sep + str(datetime.datetime.now()) + ".jpg", image)
+            counter += 1
+
+        print("Done.")
 
 
 if __name__ == "__main__":
