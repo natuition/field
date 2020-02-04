058c4f1946845fac36093d6608a463cdc1ef33fb
avc94 <>
2020-02-04 16:15:35 +0200

    Added script for separate testings of camera and detection modules

---
+++ v2_test_det_cam.py
@@ -0,0 +1,45 @@
+import detection
+import cv2 as cv
+import adapters
+
+
+def test_detection():
+    detector = detection.YoloOpenCVDetection()
+    img = cv.imread("1.jpg")
+    i = 1
+
+    while True:
+        plants = detector.detect(img)
+        print("Detected", len(plants), "plants on", str(i), "iteration")
+        i += 1
+
+
+def test_camera():
+    camera = adapters.CameraAdapterIMX219_170()
+    i = 0
+
+    while True:
+        image = camera.get_image()
+        _ = image.shape[0]
+        i += 1
+        if i % 100 == 0:
+            print(str(i), "frames read")
+
+
+def test_both():
+    detector = detection.YoloOpenCVDetection()
+    i = 1
+
+    with adapters.CameraAdapterIMX219_170() as camera:
+        while True:
+            frame = camera.get_image()
+            plants = detector.detect(frame)
+            print("Detected", len(plants), "plants on", str(i), "iteration")
+            i += 1
+
+
+if __name__ == '__main__':
+    # should be called only one of them:
+    test_detection()
+    #test_camera()
+    #test_both()
