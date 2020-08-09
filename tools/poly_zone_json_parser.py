ce54a54a5afee4a0b7001a8c6c16f24f929f21d6
avc94 <>
2020-08-09 21:13:03 +0300

    Updated json poly zone parser

--- tools/poly_zone_json_parser.py
+++ tools/poly_zone_json_parser.py
@@ -1,12 +1,15 @@
 """Tool for parsing json file which contains points of some polygon and zipping them into one list.
 Example: x = [1, 2, 3]; y = [4, 5, 6] will be converted into z = [[1, 4], [2, 5], [3, 6]]
-Aso note: script will extract and use FIRST polygon and probably won't work with the rest shapes"""
+Aso note: script will extract and use FIRST polygon and won't work with the rest shapes"""
 
 import json
 
+INPUT_FILE = "1.json"
+OUTPUT_FILE = "1.txt"
+
 
 def main():
-    with open("1.json") as file:
+    with open(INPUT_FILE) as file:
         data = json.loads(file.read())["_via_img_metadata"]
         for file_key in data:
             regions = data[file_key]["regions"]
@@ -15,10 +18,16 @@ def main():
                 points_y = region["shape_attributes"]["all_points_y"]
                 break
             break
+
+    print("Len X:", len(points_x), "Len Y:", len(points_y))
     res = []
     for el in zip(points_x, points_y):
         res.append(list(el))
+    print("Len RES:", len(res))
     print(res)
+
+    with open(OUTPUT_FILE, "w") as file:
+        file.write(str(res))
 
 
 if __name__ == '__main__':
