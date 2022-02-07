015c59fd880d1c4ebed7b1c9fbf50f33f36e69db
Vincent LAMBERT <v.lambert@natuition.com>
2022-02-07 23:29:14 +0100

    Change for x_y offset

--- deployement/cameraCalibration.py
+++ deployement/cameraCalibration.py
@@ -10,6 +10,7 @@ import grp
 import sys
 sys.path.append('../')
 from config import config
+from extraction import ExtractionManagerV3
 import configDeployment
 import utility
 import adapters
@@ -44,15 +45,16 @@ class CameraCalibration:
         CAMERA_W = 3264
         CAMERA_H = 2464
         CAMERA_FRAMERATE = 21
-        with adapters.CameraAdapterIMX219_170(  0, CAMERA_W, 0, CAMERA_H,
-                                                config.CV_ROTATE_CODE,
-                                                config.ISP_DIGITAL_GAIN_RANGE_FROM,
-                                                config.ISP_DIGITAL_GAIN_RANGE_TO,
-                                                config.GAIN_RANGE_FROM, config.GAIN_RANGE_TO,
-                                                config.EXPOSURE_TIME_RANGE_FROM, config.EXPOSURE_TIME_RANGE_TO,
-                                                config.AE_LOCK, 
-                                                CAMERA_W, CAMERA_H, CAMERA_W, CAMERA_H, CAMERA_FRAMERATE,
-                                                config.CAMERA_FLIP_METHOD) as camera:
+        with    adapters.CameraAdapterIMX219_170(  0, CAMERA_W, 0, CAMERA_H,
+                                                    config.CV_ROTATE_CODE,
+                                                    config.ISP_DIGITAL_GAIN_RANGE_FROM,
+                                                    config.ISP_DIGITAL_GAIN_RANGE_TO,
+                                                    config.GAIN_RANGE_FROM, config.GAIN_RANGE_TO,
+                                                    config.EXPOSURE_TIME_RANGE_FROM/5, config.EXPOSURE_TIME_RANGE_TO/5,
+                                                    config.AE_LOCK, 
+                                                    CAMERA_W, CAMERA_H, CAMERA_W, CAMERA_H, CAMERA_FRAMERATE,
+                                                    config.CAMERA_FLIP_METHOD) as camera, \
+                adapters.SmoothieAdapter(self.__get_smoothie_vesc_addresses()) as smoothie:
             frame = camera.get_image()
             img_origine = frame.copy()
             frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
@@ -69,6 +71,35 @@ class CameraCalibration:
                 print("Warning we found multiple circles.")
             for i in all_circles_rounded[0, :]:
                 cv2.circle(img_origine, (i[0],i[1]),i[2],(50,200,200),5)
+
+            all_circles = cv2.HoughCircles(frame,cv2.HOUGH_GRADIENT,0.9, 2500, param1 = 30, param2 = 10, minRadius = 50, maxRadius = 70)
+            all_circles_rounded = np.uint16(np.around(all_circles))
+            print('I have found ' + str(all_circles_rounded.shape[1]) + ' circles')
+            if len(all_circles_rounded) == 1:
+                self.target_x = float(all_circles_rounded[0][0][0])
+                self.target_y = float(all_circles_rounded[0][0][1])
+                self.target_radius = all_circles_rounded[0][0][2]
+            else:
+                print("Warning we found multiple circles.")
+            for i in all_circles_rounded[0, :]:
+                cv2.circle(img_origine, (i[0],i[1]),i[2],(50,200,200),5)
+                cv2.circle(img_origine, (i[0],i[1]),2,(50,200,200),5)
+
+            if ExtractionManagerV3.is_point_in_circle(self.target_x, self.target_y, self.scene_center_x, self.scene_center_y, config.UNDISTORTED_ZONE_RADIUS):
+                print("ok")
+                x = float(abs((self.target_x-self.scene_center_x)/config.ONE_MM_IN_PX))
+                y = float(abs((self.target_y-self.scene_center_y)/config.ONE_MM_IN_PX))
+                res = smoothie.custom_separate_xy_move_to(  X_F=config.X_F_MAX,
+                                                            Y_F=config.Y_F_MAX,
+                                                            X=smoothie.smoothie_to_mm(x, "X"),
+                                                            Y=smoothie.smoothie_to_mm(y, "Y"))
+                if res != smoothie.RESPONSE_OK:
+                    msg = "INIT: Failed to move camera, smoothie response:\n" + res
+                    print(msg)
+                    exit(1)
+                smoothie.wait_for_all_actions_done()
+            else:
+                print("nok")
 
             image_saver.save_image(img_origine, "./", specific_name="scene_center")
 
@@ -97,7 +128,9 @@ class CameraCalibration:
                     print(path + " = " + str(value), end='\n')
                 else:
                     print(line, end='')
-        os.chown("../config/config.py", -1, -1)
+        uid = pwd.getpwnam("violette").pw_uid
+        gid = grp.getgrnam("violette").gr_gid
+        os.chown("../config/config.py", uid, gid)
 
     def set_crop_values(self):
         rectX = self.scene_center_x - configDeployment.CAMERA_DISPLAY_W/2 - configDeployment.OFFSET_SCENE_CENTER_CENTER_CROP_VALUE_W
@@ -114,11 +147,11 @@ class CameraCalibration:
 
 def main():
     cameraCalibration: CameraCalibration = CameraCalibration()
-    cameraCalibration.step1()
+    """cameraCalibration.step1()
     test_continue = input("Press enter to continue the calibration, type anything to exit.")
     if test_continue != "":
         return
-    cameraCalibration.step1_validate()
+    cameraCalibration.step1_validate()"""
     cameraCalibration.step2()
 
 if __name__ == "__main__":
