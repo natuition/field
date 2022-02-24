c3bb544d3970c8f1e3486b29811c6afd063a3710
Vincent LAMBERT <v.lambert@natuition.com>
2022-02-24 14:15:25 +0100

    Change for x_y_dir calibration

--- deployement/deploy.py
+++ deployement/deploy.py
@@ -3,6 +3,10 @@ from flask import Flask, send_from_directory, make_response, render_template
 from flask_socketio import SocketIO, emit
 from engineio.payload import Payload
 import re
+import fileinput
+import pwd
+import grp
+import os
 
 import sys
 sys.path.append('../')
@@ -53,6 +57,21 @@ def on_x_y_dir(data):
         smoothie.custom_move_for(A_F=config.A_F_MAX, A=data["a"])
         print(f"a:{data['a']}")
     if "inv" in data:
-        
+        axis_to_config = {"X":config.X_COEFFICIENT_TO_MM,"Y":config.Y_COEFFICIENT_TO_MM,"A":config.A_COEFFICIENT_TO_MM}
+        for axis, invert in data["inv"].items():
+            if invert:
+                changeConfigValue(f"{axis}_COEFFICIENT_TO_MM",axis_to_config[axis])
+
+def changeConfigValue(path: str, value):
+    with fileinput.FileInput("../config/config.py", inplace=True, backup='.bak') as file:
+        for line in file:
+            if path in line:
+                print(path + " = " + str(value), end='\n')
+            else:
+                print(line, end='')
+    uid = pwd.getpwnam("violette").pw_uid
+    gid = grp.getgrnam("violette").gr_gid
+    os.chown("../config/config.py", uid, gid)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="80",debug=True, use_reloader=False)
