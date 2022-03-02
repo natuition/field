0dc34412a20d321d26913a25c41ec93cf5b772bb
Vincent LAMBERT <v.lambert@natuition.com>
2022-03-02 17:10:48 +0100

    Devlop of deploy server

--- deployement/deploy.py
+++ deployement/deploy.py
@@ -7,6 +7,10 @@ import fileinput
 import pwd
 import grp
 import os
+import logging
+import time
+import serial
+import json
 
 import importlib
 import sys
@@ -20,6 +24,10 @@ import adapters
 import utility
 
 app = Flask(__name__)
+app.config['DEBUG'] = False
+app.logger.disabled = True
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 Payload.max_decode_packets = 500
 socketio = SocketIO(app, async_mode=None, logger=False, engineio_logger=False)
@@ -43,29 +51,32 @@ def vesc_foc():
 @app.route("/x_y_dir")
 def x_y_dir():
     global smoothie
-    if smoothie is None:
+    print(smoothie, in_reset)
+    if smoothie is None and not in_reset:
         smoothie = adapters.SmoothieAdapter(utility.get_smoothie_vesc_addresses()["smoothie"], calibration_at_init=False)
-    return render_template('x_y_dir.html', A_MAX=config.A_MAX, Y_MAX=config.Y_MAX, X_MAX=config.X_MAX)
+    print(smoothie)
+    return render_template('x_y_dir.html', A_MAX=config.A_MAX, Y_MAX=config.Y_MAX, X_MAX=config.X_MAX, IN_RESET=json.dumps(in_reset))
 
 @socketio.on('x_y_dir', namespace='/server')
 def on_x_y_dir(data):
     global smoothie
-    if smoothie is None:
-        smoothie = adapters.SmoothieAdapter(utility.get_smoothie_vesc_addresses()["smoothie"], calibration_at_init=False)
-    if "x" in data:
+    global in_reset
+    if "x" in data and not in_reset:
         smoothie.custom_move_for(X_F=config.X_F_MAX, X=data["x"])
         print(f"x:{data['x']}")
-    if "y" in data:
+    if "y" in data and not in_reset:
         smoothie.custom_move_for( Y_F=config.Y_F_MAX, Y=data["y"])
         print(f"y:{data['y']}")
-    if "a" in data:
+    if "a" in data and not in_reset:
         smoothie.custom_move_for(A_F=config.A_F_MAX, A=data["a"])
         print(f"a:{data['a']}")
-    if "inv" in data:
-        translate_axis_grec = {"X":"alpha_dir_pin","Y":"beta_dir_pin","A":"delta_dir_pin"}
+    if "inv" in data and not in_reset:
+        translate_axis_grec = {"x":"alpha_dir_pin","y":"beta_dir_pin","a":"delta_dir_pin"}
         for axis, invert in data["inv"].items():
             if invert:
-                smoothie.get_connector().write(f"config-get sd {translate_axis_grec[axis.lower()]}")
+                cmd = f"config-get sd {translate_axis_grec[axis.lower()]}"
+                #print(cmd)
+                smoothie.get_connector().write(cmd)
                 response = smoothie.get_connector().read_some()
                 matches = re.findall(f"{translate_axis_grec[axis.lower()]} is set to (.*?)\n", response)
                 if matches:
@@ -75,9 +86,31 @@ def on_x_y_dir(data):
                     else:
                         value = value+"!"
                     cmd = f"config-set sd {translate_axis_grec[axis.lower()]} {value}"
+                    #print(cmd)
                     smoothie.get_connector().write(cmd)
                     response = smoothie.get_connector().read_some()
-                    print(response.replace("\n",""))
+                    #print(response.replace("\n",""))
+        in_reset = True
+        smoothie.get_connector().write("reset")
+        response = smoothie.get_connector().read_some()
+        #print(response.replace("\n",""))
+        smoothie.disconnect()
+        smoothie = None
+        print("Reset wait 30 sec")
+        time.sleep(30)
+        print("Search smoothie...")
+        while True:
+            print(utility.get_smoothie_vesc_addresses())
+            if "smoothie" in utility.get_smoothie_vesc_addresses():
+                if smoothie is None:
+                    print("Smoothie find.")
+                    in_reset = False
+                    time.sleep(2)
+                    socketio.emit('reload', {"reload": True} , namespace='/server', broadcast=True)
+                    break
+            else:
+                time.sleep(0.5)
+        in_reset = False
 
 def changeConfigValue(path: str, value):
     with fileinput.FileInput("../config/config.py", inplace=True, backup='.bak') as file:
@@ -91,4 +124,6 @@ def changeConfigValue(path: str, value):
     os.chown("../config/config.py", uid, gid)
 
 if __name__ == "__main__":
+    global in_reset
+    in_reset = False
     app.run(host="0.0.0.0",port="80",debug=True, use_reloader=False)
