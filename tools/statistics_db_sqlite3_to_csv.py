57bdfcebb8b4f9c2b9aab65b5b499197135fb431
avc94 <>
2022-06-16 16:15:45 +0300

    Updated sqlite3 statistics to csv script

--- tools/statistics_db_sqlite3_to_csv.py
+++ tools/statistics_db_sqlite3_to_csv.py
@@ -13,11 +13,25 @@ def main():
         db_cursor = db_connection.cursor()
 
         # get data from DB
-        sql_query = """SELECT * FROM working_times;"""
-        db_cursor.execute(sql_query)
-        data = db_cursor.fetchall()
-        sql_query = """PRAGMA table_info(working_times);"""
-        db_cursor.execute(sql_query)
+        working_times_sql = "SELECT * FROM working_times;"
+        db_cursor.execute(working_times_sql)
+        working_times_data = db_cursor.fetchall()
+
+        delta_scans_amount_sql = "SELECT COUNT(*) FROM delta_scans;"
+        db_cursor.execute(delta_scans_amount_sql)
+        delta_scans_amount = db_cursor.fetchone()
+
+        delta_scans_total_time_sql = "SELECT SUM(time_spent) FROM delta_scans;"
+        db_cursor.execute(delta_scans_total_time_sql)
+        delta_scans_total_time = db_cursor.fetchone()
+
+        delta_scans_total_shifts_sql = "SELECT SUM(shifts_count) FROM delta_scans;"
+        db_cursor.execute(delta_scans_total_shifts_sql)
+        delta_scans_total_shifts = db_cursor.fetchone()
+
+        # get tables structure
+        tables_structure_sql = "PRAGMA table_info(working_times);"
+        db_cursor.execute(tables_structure_sql)
         headers = db_cursor.fetchall()
 
         # write data to csv file
@@ -28,8 +42,8 @@ def main():
                 header_line += str(header[1]) + ","
             csv_file.write(header_line[:-1] + "\n")
 
-            # write data
-            for row in data:
+            # write working_times table's data
+            for row in working_times_data:
                 data_line = ""
                 for item in row:
                     if item == "":
@@ -38,6 +52,11 @@ def main():
                         item = round(item, 3)
                     data_line += str(item) + ","
                 csv_file.write(data_line[:-1] + "\n")
+
+            # write delta_scans table's data
+            csv_file.write("\n")
+            csv_file.write("Delta scans amount,Delta scans total time,Delta scans total shifts\n")
+            csv_file.write(f"{str(delta_scans_amount[0])},{str(delta_scans_total_time[0])},{str(delta_scans_total_shifts[0])}")
 
         print("Done!")
 
