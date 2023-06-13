9f13c1a5f765df6fbc1f78a26869ea13023dc73c
Vincent LAMBERT <v.lambert@natuition.com>
2023-06-13 15:10:36 +0200

    remove bug in browser console

--- uiWebRobot/static/js/slider.js
+++ uiWebRobot/static/js/slider.js
@@ -1,23 +1,23 @@
 function range(range_name, span_name) {
-    let span=document.getElementById(span_name);
-    let rng=document.getElementById(range_name); 
-    setValueNumberOfRange(rng.value, span);
+    let span = document.getElementById(span_name);
+    let rng = document.getElementById(range_name);
+    if (rng !== null) setValueNumberOfRange(rng.value, span);
 }
 
-function setValueNumberOfRange(number, span){
-    if(number < 100){
-        if(number < 10){
-            if(span.classList.contains("no_format")) span.innerHTML=number.toString();
-            else span.innerHTML="&ensp;" + number.toString();
-        }else{
-            if(span.classList.contains("no_format")) span.innerHTML=number.toString();
-            else span.innerHTML="&nbsp;" + number.toString();
+function setValueNumberOfRange(number, span) {
+    if (number < 100) {
+        if (number < 10) {
+            if (span.classList.contains("no_format")) span.innerHTML = number.toString();
+            else span.innerHTML = "&ensp;" + number.toString();
+        } else {
+            if (span.classList.contains("no_format")) span.innerHTML = number.toString();
+            else span.innerHTML = "&nbsp;" + number.toString();
         }
-    }else{
-        span.innerHTML=number;
+    } else {
+        span.innerHTML = number;
     }
-    if(document.getElementsByName("Newfield")[0] !== undefined) if(document.getElementsByName("Newfield")[0].id == "ValidateZone"){
+    if (document.getElementsByName("Newfield")[0] !== undefined) if (document.getElementsByName("Newfield")[0].id == "ValidateZone") {
         sliderValue = document.getElementById("r1").value;
-        socketio.emit('data', {type: "modifyZone", value : sliderValue});
+        socketio.emit('data', { type: "modifyZone", value: sliderValue });
     }
 }
