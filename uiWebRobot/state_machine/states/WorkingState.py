38a2e8ee3bd2680952e45a286c8543e1af64c3cb
Vincent LAMBERT <v.lambert@natuition.com>
2023-06-27 13:36:15 +0200

    correction bug list index

--- uiWebRobot/state_machine/states/WorkingState.py
+++ uiWebRobot/state_machine/states/WorkingState.py
@@ -156,15 +156,13 @@ class WorkingState(State.State):
                 last_gps_list_file = data["last_gps_list_file"]
                 with open("../" + last_gps_list_file, "r") as gps_his_file:
                     all_points = list()
-                    index = 0
-                    all_points[index] = list()
+                    all_points.append(list())
                     for line in gps_his_file.readlines():
                         if line.startswith("[") and line.endswith("]\n"):
                             parsed_point = line[1:-1].split(", ")
-                            all_points[index].append([float(parsed_point[1]), float(parsed_point[0])])
+                            all_points[-1].append([float(parsed_point[1]), float(parsed_point[0])])
                         else:
-                            index += 1
-                            all_points[index] = list()
+                            all_points.append(list())
                 self.socketio.emit('updateLastPath', 
                                    json.dumps(all_points), 
                                    namespace='/map',
