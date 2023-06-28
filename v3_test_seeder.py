77e1bfbf81331daf2f557607b5a70aa535218ff9
violette <violette@violette.lan>
2023-06-28 10:44:35 +0200

    Update script test seeder

--- v3_test_seeder.py
+++ v3_test_seeder.py
@@ -9,14 +9,15 @@ def wait_for_actions_done(smoothie_connector):
     return smoothie_connector.read_some()
 
 def execute_gcode_and_wait(g_code, sm_con):
-    print(g_code, " - ", end="")
+    #print(g_code, " - ", end="")
     sm_con.write(g_code)
     res = sm_con.read_some()
-    print(res)
+    #print(res)
     wait_for_actions_done(sm_con)
     
 def main():
     smoothie_vesc_addr = utility.get_smoothie_vesc_addresses()
+    min_f_x_y = config.Y_F_MAX if (config.Y_F_MAX < config.X_F_MAX) else config.X_F_MAX
     if config.SMOOTHIE_BACKEND == 1:
         smoothie_address = config.SMOOTHIE_HOST
     else:
@@ -33,54 +34,50 @@ def main():
         res = sm_con.read_some()
         print("G91 - ", res)
 
-        sm_con.write("M280 S2")
+        sm_con.write(f"M280 S{config.SEEDER_CLOSE_COMMAND}")
         res = sm_con.read_some()
-        print("M280 S2 - ", res)
+        print(f"M280 S{config.SEEDER_CLOSE_COMMAND} - ", res)
 
         # go home
-        g_code = "G28 XY F30000"
+        g_code = f"G28 XY F{min_f_x_y}"
         execute_gcode_and_wait(g_code, sm_con)
 
         # go middle
-        g_code = "G0 X"+ str(config.X_MAX/2) +" F30000"
+        g_code = "G0 X"+ str(config.X_MAX/2) + " Y"+ str(config.Y_MAX/2) +f" F{min_f_x_y}"
         execute_gcode_and_wait(g_code, sm_con)
 
-        i = 50
+        i = 1000
 
         while i>0:
             try:
                 print(i)
 
-                user_cmd = input("Press enter to make seeder cycle")
-                if user_cmd != "":
-                    break
-
                 # go up
-                g_code = "G0 Y"+ str(config.Y_MAX/2) +" F30000"
+                g_code = "G0 Y"+ str(config.Y_MAX/2) +f" F{config.Y_F_MAX}"
                 execute_gcode_and_wait(g_code, sm_con)
 
                 # open seeder
-                g_code = "M280 S5.5"
+                g_code = f"M280 S{config.SEEDER_OPEN_COMMAND}"
                 execute_gcode_and_wait(g_code, sm_con)
 
                 time.sleep(0.5)
 
                 # close seeder
-                g_code = "M280 S2"
+                g_code = f"M280 S{config.SEEDER_CLOSE_COMMAND}"
                 execute_gcode_and_wait(g_code, sm_con)
 
                 time.sleep(0.5)
 
                 # shake
-                g_code = "G0 X-1Y-1 F30000"
+                g_code = f"G0 X-1Y-1 F{min_f_x_y}"
                 execute_gcode_and_wait(g_code, sm_con)
-                g_code = "G0 X2Y2 F30000"
+                g_code = f"G0 X2Y2 F{min_f_x_y}"
                 execute_gcode_and_wait(g_code, sm_con)
-                g_code = "G0 X-1Y-1 F30000"
+                g_code = f"G0 X-1Y-1 F{min_f_x_y}"
                 execute_gcode_and_wait(g_code, sm_con)
 
                 # go down
-                g_code = "G0 Y-"+ str(config.Y_MAX/2) +" F30000"
+                g_code = "G0 Y-"+ str(config.Y_MAX/2) +f" F{config.Y_F_MAX}"
                 execute_gcode_and_wait(g_code, sm_con)
 
                 i-=1;
