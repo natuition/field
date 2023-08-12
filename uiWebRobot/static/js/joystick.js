dfe534c12d190829a61d7032916b827f7a34a230
Vincent LAMBERT <v.lambert@natuition.com>
2023-08-12 12:51:08 +0200

    added the fact that you can move the robot with pc arrows

--- uiWebRobot/static/js/joystick.js
+++ uiWebRobot/static/js/joystick.js
@@ -4,6 +4,8 @@ socketio.on("reconnect_attempt", (attempt) => {
 });
 
 var joystick_created = false;
+var use_arrow = 0;
+var x_y_arrow = [0, 0];
 
 function createJoystick() {
     if (!joystick_created && getComputedStyle(document.getElementById('main')).display != "none") {
@@ -13,8 +15,13 @@ function createJoystick() {
         var isInCenter = false;
 
         setInterval(function () {
-            var xCurrentValue = joy.GetX();
-            var yCurrentValue = joy.GetY();
+            if (use_arrow > 0) {
+                var xCurrentValue = x_y_arrow[0];
+                var yCurrentValue = x_y_arrow[1];
+            } else {
+                var xCurrentValue = joy.GetX();
+                var yCurrentValue = joy.GetY();
+            }
             let style_main = getComputedStyle(document.getElementById('main')).display
             if (!document.getElementById('canvas_joystick').classList.contains("disable") && style_main != 'none') {
                 if (xCurrentValue != 0 || yCurrentValue != 0 || !isInCenter) {
@@ -33,3 +40,78 @@ createJoystick();
 window.addEventListener("orientationchange", function () {
     createJoystick();
 }, false);
+
+
+use_arrow_dir = [0, 0, 0, 0];
+
+document.onkeydown = function (e) {
+    if (!document.getElementById('canvas_joystick').classList.contains('disable')) {
+        switch (e.key) {
+            case "ArrowLeft":
+                x_y_arrow[0] = -100;
+                if (use_arrow_dir[0] == 0) {
+                    use_arrow_dir[0] = 1;
+                    use_arrow += 1;
+                }
+                break;
+
+            case "ArrowUp":
+                x_y_arrow[1] = 100;
+                if (use_arrow_dir[1] == 0) {
+                    use_arrow_dir[1] = 1;
+                    use_arrow += 1;
+                }
+                break;
+
+            case "ArrowRight":
+                x_y_arrow[0] = 100;
+                if (use_arrow_dir[2] == 0) {
+                    use_arrow_dir[2] = 1;
+                    use_arrow += 1;
+                }
+                break;
+
+            case "ArrowDown":
+                x_y_arrow[1] = -100;
+                if (use_arrow_dir[3] == 0) {
+                    use_arrow_dir[3] = 1;
+                    use_arrow += 1;
+                }
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+    }
+
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+};
+
+document.onkeyup = function (e) {
+    if (!document.getElementById('canvas_joystick').classList.contains('disable')) {
+        switch (e.key) {
+            case "ArrowLeft":
+                x_y_arrow[0] = 0;
+                use_arrow_dir[0] = 0;
+                break;
+
+            case "ArrowUp":
+                x_y_arrow[1] = 0;
+                use_arrow_dir[1] = 0;
+                break;
+
+            case "ArrowRight":
+                x_y_arrow[0] = 0;
+                use_arrow_dir[2] = 0;
+                break;
+
+            case "ArrowDown":
+                x_y_arrow[1] = 0;
+                use_arrow_dir[3] = 0;
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+    }
+    use_arrow -= 1;
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+};
