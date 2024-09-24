b8a3659c12434c564048e82cc9cad00d636db404
Julie Raud <julie.raud@etudiant.univ-lr.fr>
2024-09-24 14:27:50 +0200

    Change calibration order

--- config/config_v20_defaults.py
+++ config/config_v20_defaults.py
@@ -322,7 +322,7 @@ C_AXIS_CALIBRATION_TO_MAX = None
 CALIBRATION_DISTANCE = 1000  # should be always positive, sign will be auto-defined using *_AXIS_CALIBRATION_TO_MAX flag key
 AFTER_CALIBRATION_AXIS_OFFSET = 0
 CORK_CALIBRATION_MIN_TIME = 3600 
-CALIBRATION_ORDER = ["Z", "X", "Y", "A", "B", "C"]
+CALIBRATION_ORDER = ["Z", "Y", "X", "A", "B", "C"]
 
 # DIRECTION WHEELS
 A_MIN = -6 
