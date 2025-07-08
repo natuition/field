18edd41edcf1bfcc11f69a7f9e8eacdf44a3b976
Melvin WARNET <melvin.warnet@gmail.com>
2025-07-08 15:14:17 +0200

    FIX : voltage indicator doesn't change buttons status will main

--- uiWebRobot/static/js/voltage_indicator.js
+++ uiWebRobot/static/js/voltage_indicator.js
@@ -37,60 +37,68 @@ socketVoltage.on('update', function (data) {
 
 // Fonction pour desactiver les boutons 
 function disableButtons() {
-    const continue_button = document.getElementById("Continue");
-    if (continue_button) {
-        continue_button.classList.add("disabled");
-        continue_button.setAttribute("disabled", "disabled");
-    }
-    const start_button = document.getElementById("Start");
-    if (start_button) {
-        start_button.classList.add("disabled");
-        start_button.setAttribute("disabled", "disabled");
-    }
-    const newfield_button = document.getElementById("Newfield");
-    if (newfield_button) {
-        newfield_button.classList.add("disabled");
-        newfield_button.setAttribute("disabled", "disabled");
-    }
-    const joystick_button = document.getElementById("canvas_joystick");
-    if (joystick_button) {
-        joystick_button.classList.add("disable");
-    }
-    const checklist_button = document.getElementById("checkbutton");
-    if (checklist_button) {
-        checklist_button.classList.add("disabled");
-        checklist_button.setAttribute("disabled", "disabled");
+    const pieChart = document.getElementById("pieChart");
+    if (!pieChart) {
+        const continue_button = document.getElementById("Continue");
+        if (continue_button) {
+            if (section)
+            continue_button.classList.add("disabled");
+            continue_button.setAttribute("disabled", "disabled");
+        }
+        const start_button = document.getElementById("Start");
+        if (start_button) {
+            start_button.classList.add("disabled");
+            start_button.setAttribute("disabled", "disabled");
+        }
+        const newfield_button = document.getElementById("Newfield");
+        if (newfield_button) {
+            newfield_button.classList.add("disabled");
+            newfield_button.setAttribute("disabled", "disabled");
+        }
+        const joystick_button = document.getElementById("canvas_joystick");
+        if (joystick_button) {
+            joystick_button.classList.add("disable");
+        }
+        const checklist_button = document.getElementById("checkbutton");
+        if (checklist_button) {
+            checklist_button.classList.add("disabled");
+            checklist_button.setAttribute("disabled", "disabled");
+        }
     }
 }
 
 // Fonction pour activer les boutons
 function enableButtons() {
-    const continue_button = document.getElementById("Continue");
-    if (continue_button) {
-        continue_button.classList.remove("disabled");
-        continue_button.removeAttribute("disabled");
-    }
-    const start_button = document.getElementById("Start");
-    if (start_button) {
-        start_button.classList.remove("disabled");
-        start_button.removeAttribute("disabled");
-    }
-    const newfield_button = document.getElementById("Newfield");
-    if (newfield_button) {
-        newfield_button.classList.remove("disabled");
-        newfield_button.removeAttribute("disabled");
-    }
-    const joystick_button = document.getElementById("canvas_joystick");
-    if (joystick_button) {
-        joystick_button.classList.remove("disable");
-    }
-    if (typeof canNext === "function") {
-        if (canNext()) {
-            const checklist_button = document.getElementById("checkbutton");
-            if (checklist_button) {
-                checklist_button.classList.remove("disabled");
-                checklist_button.removeAttribute("disabled");
+    const pieChart = document.getElementById("pieChart");
+    if (!pieChart) {
+        const continue_button = document.getElementById("Continue");
+        if (continue_button) {
+            continue_button.classList.remove("disabled");
+            continue_button.removeAttribute("disabled");
+        }
+        const start_button = document.getElementById("Start");
+        if (start_button) {
+            start_button.classList.remove("disabled");
+            start_button.removeAttribute("disabled");
+        }
+        const newfield_button = document.getElementById("Newfield");
+        if (newfield_button) {
+            newfield_button.classList.remove("disabled");
+            newfield_button.removeAttribute("disabled");
+        }
+        const joystick_button = document.getElementById("canvas_joystick");
+        if (joystick_button) {
+            joystick_button.classList.remove("disable");
+        }
+        if (typeof canNext === "function") {
+            if (canNext()) {
+                const checklist_button = document.getElementById("checkbutton");
+                if (checklist_button) {
+                    checklist_button.classList.remove("disabled");
+                    checklist_button.removeAttribute("disabled");
+                }
             }
         }
     }
+    
 }
