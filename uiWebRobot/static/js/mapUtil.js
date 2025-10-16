500dce7e1aa9b8898c8cc84e1ef370cbcb0353b6
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2025-10-16 13:24:54 +0200

    check why mapUtil didn't work

--- uiWebRobot/static/js/mapUtil.js
+++ uiWebRobot/static/js/mapUtil.js
@@ -539,6 +539,7 @@ function updateDisplayInstructionPathLayer(map, dataServ) {
 }
 
 socketMap.on('updateDisplayInstructionPath', function (dataServ) {
+    print("dataServ: ", dataServ)
     updateDisplayInstructionPathLayer(map, dataServ);
 });
 
