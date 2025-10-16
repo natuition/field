1a4fcdfed12e7f6105a5537c1e3db9ca31b2c27d
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2025-10-16 10:39:51 +0200

    movve NAVIGATION TEST MODE SETTINGS at top

--- config/config_v20_defaults.py
+++ config/config_v20_defaults.py
@@ -9,6 +9,7 @@ CONFIG_VERSION = "2.2.2"
 
 # NAVIGATION ROUTING SETTINGS
 # TRAJECTORY PLANNER SETTINGS
+# NAVIGATION TEST MODE SETTINGS
 # EXTRACTION SETTINGS
 # VESC SETTINGS
 # PENETROMETRY SETTINGS
@@ -26,7 +27,6 @@ CONFIG_VERSION = "2.2.2"
 # CAMERA SETTINGS
 # PATHS SETTINGS
 # PREDICTION SETTINGS
-# NAVIGATION TEST MODE SETTINGS
 # PHYSICAL BLOCAGE SETTINGS
 # UNSORTED KEYS
 # ======================================================================================================================
@@ -151,6 +151,22 @@ USE_SMOOTH_APPROACHING_TO_FIELD = True
 # previous points than this value (default value for bezier path is NUMBER_OF_BEZIER_POINT * 4 + 10;
 # default value for bezier with filled corners *not implemented yet*)
 SMOOTH_APPROACHING_MAX_POINTS = NUMBER_OF_BEZIER_POINT * 4 + 10
+
+
+# ======================================================================================================================
+# NAVIGATION TEST MODE SETTINGS
+# ======================================================================================================================
+DISPLAY_INSTRUCTION_PATH = True #Allows to display the robot guide points on the ui.
+DELTA_DISPLAY_INSTRUCTION_PATH = 15 #Number of guide points display on the ui.
+
+NAVIGATION_TEST_MODE = False # mode allowing the robot to do A->B, B->A
+#The robot will aim for the furthest point, 
+#when it reaches this point it will wait for a press on enter to go to the furthest point from it.
+POINT_A = [[46.1546931, -1.1198362], -0.5] #Point coordinate for test navigation mode, [[lat,long],speed]
+# the speed represents the speed the robot will apply to reach this point.
+POINT_B = [[46.1545618, -1.119885], 0.5] #Point coordinate for test navigation mode, [[lat,long],speed]
+# the speed represents the speed the robot will apply to reach this point.
+RELOAD_CONFIG_DURING_NAVIGATION_TEST = False
 
 
 # ======================================================================================================================
@@ -696,21 +712,6 @@ STATISTICS_DB_FILE_NAME = "statistics_db.sqlite3"
 # PREDICTION SETTINGS
 # ======================================================================================================================
 ZONE_THRESHOLD_DEGREE = [(436,5),(697,7),(796,17),(849,15),(953,6)]
-
-
-# ======================================================================================================================
-# NAVIGATION TEST MODE SETTINGS
-# ======================================================================================================================
-NAVIGATION_TEST_MODE = False # mode allowing the robot to do A->B, B->A
-#The robot will aim for the furthest point, 
-#when it reaches this point it will wait for a press on enter to go to the furthest point from it.
-DISPLAY_INSTRUCTION_PATH = False #Allows to display the robot guide points on the ui.
-DELTA_DISPLAY_INSTRUCTION_PATH = 15 #Number of guide points display on the ui.
-POINT_A = [[46.1546931, -1.1198362], -0.5] #Point coordinate for test navigation mode, [[lat,long],speed]
-# the speed represents the speed the robot will apply to reach this point.
-POINT_B = [[46.1545618, -1.119885], 0.5] #Point coordinate for test navigation mode, [[lat,long],speed]
-# the speed represents the speed the robot will apply to reach this point.
-RELOAD_CONFIG_DURING_NAVIGATION_TEST = False
 
 
 # ======================================================================================================================
