d334ec74b014029df15fb59cc66c945f7a4f79a9
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2026-06-23 12:06:02 +0200

    fix(client): remove redundant logging for MVI pipeline id-name map

--- adapters.py
+++ adapters.py
@@ -2785,8 +2785,6 @@ class ClientMVI:
         })
         self.__check_result(res)
         result: Message = res
-        print(f"[{self.__class__.__name__}] -> MVI pipeline {new_pipeline.value} id-name map: {result}")
-        print(f"[{self.__class__.__name__}] -> MVI pipeline {new_pipeline.value} id-name map type: {type(result)}")
         self.__id_name_map[new_pipeline] = result["message"]
         print(f"[{self.__class__.__name__}] -> MVI pipeline {new_pipeline.value} id-name map: {self.__id_name_map[new_pipeline]}")
         
