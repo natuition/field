69333eeff185c33574ee77aaad2e208c2e56941d
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2026-06-23 11:30:58 +0200

    fix(client): remove redundant parameter in id-name map retrieval

--- adapters.py
+++ adapters.py
@@ -2783,8 +2783,7 @@ class ClientMVI:
         
     def __get_id_name_map(self, new_pipeline: MVIPipelineDescriptor):
         res = self.__client.call(CallType.GET,{
-            "property": MVIProperty.ID_NAME_MAP_OF_ACTIVE_PIPELINE.value,
-            "param": new_pipeline
+            "property": MVIProperty.ID_NAME_MAP_OF_ACTIVE_PIPELINE.value
         })
         self.__check_result(res)
         result: ResultDTO = res
