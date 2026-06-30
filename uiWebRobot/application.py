e64db7ec36eff5b036959d00818779b04dcbac37
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2026-06-30 12:21:07 +0200

    fix(application): improve error logging to capture full traceback

--- uiWebRobot/application.py
+++ uiWebRobot/application.py
@@ -476,5 +476,5 @@ if __name__ == "__main__":
         Logger.setLevel(config.LOG_LEVEL_UI)
         logger = Logger.create("Runtime")
         logger.error(f"Error : {e}", exc_info=True)
-        traceback.print_stack()
+        logger.error(traceback.format_exc())
         print(f"Error : {e}")
