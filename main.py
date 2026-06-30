34a5e4f73e3feeac0d3b0b4493f3b048dd166ee1
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2026-06-30 13:01:28 +0200

    refactor(logger): change log level to error for starting main function

--- main.py
+++ main.py
@@ -1369,7 +1369,7 @@ def get_bezier_indexes(path_points: list):
 
 def main():
     
-    MAIN_LOGGER.info("Starting main()")
+    MAIN_LOGGER.error("Starting main()")
     
     time_start = utility.get_current_time()
     utility.create_directories(config.LOG_ROOT_DIR)
