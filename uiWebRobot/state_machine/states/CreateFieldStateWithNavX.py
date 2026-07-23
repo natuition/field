9e51b9da3e78351e3f7d73cb4fe92f27f248b402
Vincent LAMBERT <vincent.lambertvlb@gmail.com>
2026-07-23 10:20:38 +0000

    refactor: remove unused FieldCreator class and related code

--- uiWebRobot/state_machine/states/CreateFieldStateWithNavX.py
+++ uiWebRobot/state_machine/states/CreateFieldStateWithNavX.py
@@ -45,9 +45,6 @@ class CreateFieldStateWithNavX(State.State):
                                                 joystick=True,
                                                 slider=config.SLIDER_CREATE_FIELD_DEFAULT_VALUE)
 
-        self.fieldCreator = FieldCreator(self.__file_logger, self.nav, self.vesc_engine, self.smoothie,
-                                         self.socketio)
-
         self.field = None
         self.manoeuvre = False
 
@@ -58,10 +55,7 @@ class CreateFieldStateWithNavX(State.State):
         except:
             self.notificationQueue = None
     
-        self.__ui_languages, self.__current_ui_language = utilsFunction.get_ui_language()
-
-        #self.__send_last_pos_thread_alive = True
-        #self._send_last_pos_thread = threading.Thread(target=send_last_pos_thread_tf, args=(lambda : self.send_last_pos_thread_alive, self.socketio, self.__file_logger), daemon=True)
+        # self.__ui_languages, self.__current_ui_language = utilsFunction.get_ui_language()
 
 
     def on_event(self, event):
@@ -70,26 +64,19 @@ class CreateFieldStateWithNavX(State.State):
             self.statusOfUIObject.fieldButton = ButtonState.NOT_HERE
             self.statusOfUIObject.stopButton = ButtonState.CHARGING
 
-            #self.__send_last_pos_thread_alive = False
-            #self._send_last_pos_thread.join()
+            # try:
+            #     self.fieldCreator.setSecondPoint()
+            # except TimeoutError:
+            #     if self.notificationQueue is not None:
+            #         self.notificationQueue.send(json.dumps({"message_name": "No_GPS_for_field"}))
+            #     return WaitWorkingState.WaitWorkingState(self.socketio, self.__file_logger, False, self.smoothie, self.vesc_engine)
 
-            try:
-                self.fieldCreator.setSecondPoint()
-            except TimeoutError:
-                if self.notificationQueue is not None:
-                    self.notificationQueue.send(json.dumps({"message_name": "No_GPS_for_field"}))
-                return WaitWorkingState.WaitWorkingState(self.socketio, self.__file_logger, False, self.smoothie, self.vesc_engine)
-
-            self.field = self.fieldCreator.calculateField()
-            if not config.TWO_POINTS_FOR_CREATE_FIELD and not config.FORWARD_BACKWARD_PATH:
-                self.manoeuvre = True
-                if config.MAKE_MANEUVER_AFTER_FIELD_CREATE:
-                    self.fieldCreator.manoeuvre()
-                self.manoeuvre = False
-
-            #self.__send_last_pos_thread_alive = True
-            #self._send_last_pos_thread = threading.Thread(target=send_last_pos_thread_tf, args=(lambda : self.send_last_pos_thread_alive, self.socketio, self.__file_logger), daemon=True)
-            #self._send_last_pos_thread.start()
+            # self.field = self.fieldCreator.calculateField()
+            # if not config.TWO_POINTS_FOR_CREATE_FIELD and not config.FORWARD_BACKWARD_PATH:
+            #     self.manoeuvre = True
+            #     if config.MAKE_MANEUVER_AFTER_FIELD_CREATE:
+            #         self.fieldCreator.manoeuvre()
+            #     self.manoeuvre = False
 
             self.statusOfUIObject.stopButton = ButtonState.NOT_HERE
             self.statusOfUIObject.fieldButton = ButtonState.VALIDATE
@@ -132,185 +119,3 @@ class CreateFieldStateWithNavX(State.State):
 
     def getField(self):
         return self.field
-
-
-class FieldCreator:
-
-    def __init__(self,
-                 file_logger: utility.Logger,
-                 nav: navigation.GPSComputing,
-                 vesc_engine: adapters.VescAdapterV4,
-                 smoothie: adapters.SmoothieAdapter,
-                 socketio: SocketIO):
-        self.__logger = LoggerFactory.create(self.__class__.__name__)
-        self.A = [0, 0]
-        self.B = [0, 0]
-        self.C = [0, 0]
-        self.D = [0, 0]
-        self.length_field = 0
-        self.field = []
-        self.__file_logger = file_logger
-        self.nav = nav
-        self.vesc_emergency: adapters.VescAdapterV4 = vesc_engine
-        self.smoothie = smoothie
-        self.socketio = socketio
-
-    def setFirstPoint(self):
-        msg = f"Getting point A..."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-        with adapters.GPSUbloxAdapterWithoutThread(config.GPS_PORT, config.GPS_BAUDRATE, config.GPS_POSITIONS_TO_KEEP) as gps:
-            self.A = utility.average_point(gps, None, self.nav, self.__file_logger)
-
-        self.socketio.emit('newPos', json.dumps([self.A[1], self.A[0]]), namespace='/map')
-
-        if self.vesc_emergency is None:
-            self.vesc_emergency: adapters.VescAdapterV4 = utilsFunction.initVesc(self.__file_logger)
-        msg = f"Moving forward..."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-        self.vesc_emergency.set_target_rpm(
-            config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM,
-            self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.start_moving(self.vesc_emergency.PROPULSION_KEY)
-
-    def setSecondPoint(self):
-        msg = f"Stop moving forward..."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-
-        self.vesc_emergency.stop_moving(self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.wait_for_stop(self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_target_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_current_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-
-        msg = f"Getting point B..."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-        with adapters.GPSUbloxAdapterWithoutThread(config.GPS_PORT, config.GPS_BAUDRATE, config.GPS_POSITIONS_TO_KEEP) as gps:
-            self.B = utility.average_point(gps, None, self.nav, self.__file_logger)
-
-        self.socketio.emit('newPos', json.dumps([self.B[1], self.B[0]]), namespace='/map')
-
-    def setFieldSize(self, size: int):
-        self.length_field = size
-
-    def calculateField(self):
-        width_field = self.nav.get_distance(self.A, self.B)
-
-        msg = f"Field_size : {round(self.length_field / 1000, 2)} (length) / {round(width_field / 1000, 2)} (width)."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-
-        if not config.TWO_POINTS_FOR_CREATE_FIELD:
-            self.C = self.nav.get_coordinate(self.B, self.A, 90, self.length_field)
-            self.D = self.nav.get_coordinate(self.C, self.B, 90, width_field)
-            self.field = [self.B, self.C, self.D, self.A]
-        else:
-            self.field = [self.B, self.A]
-
-        other_fields = utilsFunction.get_other_field()
-
-        link_path = os.path.realpath("./field.txt")
-        current_field_name = (link_path.split("/")[-1]).split(".")[0]
-
-        self.socketio.emit('newField', json.dumps(
-            {"field": self.formattingFieldPointsForSend(), "other_fields": other_fields,
-             "current_field_name": unquote(current_field_name, encoding='utf-8')}), namespace='/map')
-
-        return self.field
-
-    def formattingFieldPointsForSend(self):
-        coords = list()
-
-        for coord in self.field:
-            coords.append([coord[1], coord[0]])
-
-        if len(self.field) == 4:
-            coords.append(coords[0])
-
-        return coords
-
-    def saveField(self, fieldPath: str, fieldName: str):
-        cpt = 1
-        fieldName = quote(fieldName, safe="", encoding='utf-8')
-        if (os.path.exists(fieldPath + fieldName)):
-            while os.path.exists(f"{fieldPath + fieldName[:-4]}_{cpt}.txt"):
-                cpt += 1
-            fieldName = f"{fieldName[:-4]}_{cpt}.txt"
-        path = fieldPath + fieldName
-        msg = f"Save field in {path}..."
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.__logger.info(msg)
-        utilsFunction.save_gps_coordinates(self.field, path)
-        return (path, unquote(fieldName[:-4], encoding='utf-8'))
-
-    def manoeuvre(self):
-
-        # move backward and stop
-        msg = f"Field creation: starting vesc movement of " \
-                f"RPM={-config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM}."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-        
-        self.vesc_emergency.set_time_to_move(config.VESC_MOVING_TIME, self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_target_rpm(
-            -config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM,
-            self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_current_rpm(-config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM,self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.start_moving(self.vesc_emergency.PROPULSION_KEY)
-        time.sleep(config.MANEUVER_TIME_BACKWARD)
-        self.vesc_emergency.stop_moving(self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_target_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_current_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-
-        msg = f"Field creation: stopped vesc movement of " \
-                f"RPM={-config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM}."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-
-        # vesc can't stop robot instantly, so we wait for 1 sec before turn wheels right
-        time.sleep(1)
-
-        # turn wheels to right
-        msg = f"Field creation: starting turning smoothie wheels to A={config.A_MIN}."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.smoothie.custom_move_to(A_F=config.A_F_UI, A=config.A_MIN)
-        self.smoothie.wait_for_all_actions_done()
-        msg = f"Field creation: stopped turning smoothie wheels to A={config.A_MIN}."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-
-        # move forward (wheels are turned to right) and stop
-        msg = f"Field creation: starting vesc movement of " \
-                f"RPM={config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM}."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-
-        self.vesc_emergency.set_target_rpm(
-            config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM,
-            self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_current_rpm(config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM,self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.start_moving(self.vesc_emergency.PROPULSION_KEY)
-        time.sleep(config.MANEUVER_TIME_FORWARD)
-        self.vesc_emergency.stop_moving(self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_target_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-        self.vesc_emergency.set_current_rpm(0,self.vesc_emergency.PROPULSION_KEY)
-
-        msg = f"Field creation: stopped vesc movement of " \
-                f"RPM={config.SI_SPEED_UI * config.MULTIPLIER_SI_SPEED_TO_RPM}"
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-
-        # align wheels to center
-        msg = f"Field creation: starting turning smoothie wheels to A=0."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-        self.smoothie.custom_move_to(A_F=config.A_F_UI, A=0)
-        self.smoothie.wait_for_all_actions_done()
-        msg = f"Field creation: stopped turning smoothie wheels to A=0."
-        self.__logger.debug(msg)
-        self.__file_logger.write_and_flush(msg + "\n")
-
-        self.vesc_emergency.set_time_to_move(config.VESC_MOVING_TIME, self.vesc_emergency.PROPULSION_KEY)
